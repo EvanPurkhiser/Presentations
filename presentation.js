f5b5b93cee20f9a67b0ec1d8b7f1cfa7c0cf9259
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-09-03 01:10:25 -0400

    Add page numbers to presentations

--- presentation.js
+++ presentation.js
@@ -2,7 +2,7 @@
 var mdUri = 'presentations/' + window.location.search.substring(1) + '.md';
 
 // Set the uri to the presentation markdown file
-window.document.getElementById('md').setAttribute('data-markdown', mdUri);
+document.getElementById('md').setAttribute('data-markdown', mdUri);
 
 Reveal.initialize({
 
@@ -11,4 +11,30 @@ Reveal.initialize({
 	transition: 'default',           // default/cube/page/concave/zoom/linear/fade/none
 	transitionSpeed: 'default',      // default/fast/slow
 	backgroundTransition: 'default', // default/linear/none
+});
+
+// Add page numbers
+// https://github.com/hakimel/reveal.js/pull/56#issuecomment-20245613
+function slidenumber(event){
+  var totalslides = document.querySelectorAll( '.reveal .slides section:not(.stack)' ).length;
+  var current_slide = 0;
+
+  var horizontal_slides = document.querySelectorAll( '.reveal .slides>section' );
+  for (var i = 0; i < event.indexh; i++) {
+    // get subslides
+    var subslides = horizontal_slides[i].querySelectorAll('section');
+
+    // if subslides.length is 0, add 1 for horizontal slide, else add subslides.length
+    current_slide += (subslides.length === 0) ? 1 : subslides.length;
+  }
+
+  current_slide += event.indexv+1;
+  return current_slide.toString()+"/"+totalslides.toString();
+}
+
+Reveal.addEventListener('slidechanged', function(event){
+  document.querySelector(".slidenumber").innerText=slidenumber(event);
+});
+Reveal.addEventListener('ready', function(event){
+  document.querySelector(".slidenumber").innerText=slidenumber(event);
 });
